7581b45c11c59edac29d22067a24e815d9841056
gkbel46 <girish.bellenavar@gmail.com>
2017-03-06 20:54:01 +0530

    If condition now checks if the lane lines detected are null. If null polynomial_fit_1() will be used to predeict lanes

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ g_prev_right_fit =None
 def process_Video(input_image):
     global g_prev_left_fit
     global g_prev_right_fit
-    output_image , left_fit, right_fit = process_image(input_image,g_mtx,g_dst,g_prev_left_fit,g_prev_right_fit,True)
+    output_image , left_fit, right_fit = process_image(input_image,g_mtx,g_dst,g_prev_left_fit,g_prev_right_fit)
     g_prev_left_fit = left_fit
     g_prev_right_fit = right_fit
     return output_image
